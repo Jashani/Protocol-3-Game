9994b088339531e2b4cbda2f4a6a6b7194e3e2c7
Yasha Rise <yrise@gitlab.com>
2025-08-15 15:12:17 +0300

    Increase message font size

--- data.gd
+++ data.gd
@@ -6,5 +6,14 @@ func _ready() -> void:
 func save(data: Dictionary):
 	pass
 
+func save_metadata(key: String, value):
+	pass
+
+func save_demographic(key: String, value):
+	pass
+
+func save_response_data(headline_id: int, data: Dictionary):
+	pass
+
 func check_connectivity():
 	pass
